2bf53a6366df65ec955a365e7362967d7fde2ead
Azamat <azamatsalaly@gmail.com>
2024-02-20 19:34:09 +0600

    Написали и протестировали программу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program 
+{
+    static void Main()
+    {
+        string[] array = { "Урок", "Задание", "Два", "Пять", "Оценка", "Три" };
+        Console.Write($"[{String.Join(", ", array)}] => ");
+        int count = 0;
+       
+        foreach (string word in array)
+        {
+            if (word.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] res = new string[count];
+        int index = 0;
+        
+        foreach (string word in array)
+        {
+            if (word.Length <= 3)
+            {
+                res[index] = word;
+                index++;
+            }
+        }
+        Console.Write($"[{String.Join(", ", res)}]");
+    }
+}
